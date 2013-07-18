3fc1edb45808c792c7a10a2eae6d32116d49f0c8
chaosfoal <arturs.znotins@gmail.com>
2013-07-19 00:23:06 +0300

    bat files + last dist

--- export_laura_ner.bat
+++ export_laura_ner.bat
@@ -1,13 +1,29 @@
 @echo off
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_16 -conll laura_ner\interview_16.txt.tagged -exportConll laura_ner\interview_16.conll -ner -mmaxPath laura_ner\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_16 -conll laura_ner\interview_16.conll -exportConll laura_ner\interview_16_ner.conll -ner -mmaxPath laura_ner\
 
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_23 -conll laura_ner\interview_23.txt.tagged -exportConll laura_ner\interview_23.conll -ner -mmaxPath laura_ner\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_23 -conll laura_ner\interview_23.conll -exportConll laura_ner\interview_23_ner.conll -ner -mmaxPath laura_ner\
 
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_27 -conll laura_ner\interview_27.txt.tagged -exportConll laura_ner\interview_27.conll -ner -mmaxPath laura_ner\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_27 -conll laura_ner\interview_27.conll -exportConll laura_ner\interview_27_ner.conll -ner -mmaxPath laura_ner\
 
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_38 -conll laura_ner\interview_38.txt.tagged -exportConll laura_ner\interview_38.conll -ner -mmaxPath laura_ner\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_38 -conll laura_ner\interview_38.conll -exportConll laura_ner\interview_38_ner.conll -ner -mmaxPath laura_ner\
 
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_43 -conll laura_ner\interview_43.txt.tagged -exportConll laura_ner\interview_43.conll -ner -mmaxPath laura_ner\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_43 -conll laura_ner\interview_43.conll -exportConll laura_ner\interview_43_ner.conll -ner -mmaxPath laura_ner\
 
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_46 -conll laura_ner\interview_46.txt.tagged -exportConll laura_ner\interview_46.conll -ner -mmaxPath laura_ner\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_46 -conll laura_ner\interview_46.conll -exportConll laura_ner\interview_46_ner.conll -ner -mmaxPath laura_ner\
+
+
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project G_2005_Ugales_Baznica -conll laura_ner\G_2005_Ugales_Baznica.conll -exportConll laura_ner\G_2005_Ugales_Baznica_ner.conll -ner -mmaxPath laura_ner\
+
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project G_1966_Oskars_Kalpaks -conll laura_ner\G_1966_Oskars_Kalpaks.conll -exportConll laura_ner\G_1966_Oskars_Kalpaks_ner.conll -ner -mmaxPath laura_ner\
+
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project G_1934_Madona -conll laura_ner\G_1934_Madona.conll -exportConll laura_ner\G_1934_Madona_ner.conll -ner -mmaxPath laura_ner\
+
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 2007_Dadzis -conll laura_ner\2007_Dadzis.conll -exportConll laura_ner\2007_Dadzis_ner.conll -ner -mmaxPath laura_ner\
+
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 1988_Padomju_Jaunatne -conll laura_ner\1988_Padomju_Jaunatne.conll -exportConll laura_ner\1988_Padomju_Jaunatne_ner.conll -ner -mmaxPath laura_ner\
+
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 1942_Austrumu_apgabala_raiditaju_grupa -conll laura_ner\1942_Austrumu_apgabala_raiditaju_grupa.conll -exportConll laura_ner\1942_Austrumu_apgabala_raiditaju_grupa_ner.conll -ner -mmaxPath laura_ner\
+
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 1935_Muzikas_Apskats -conll laura_ner\1935_Muzikas_Apskats.conll -exportConll laura_ner\1935_Muzikas_Apskats_ner.conll -ner -mmaxPath laura_ner\
+
 pause
