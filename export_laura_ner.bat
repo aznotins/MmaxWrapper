e13758eea42b4ad8722b5577d9d7adab95ecfb9b
chaosfoal <arturs.znotins@gmail.com>
2013-08-15 01:59:47 +0300

    Added new gold NE data

--- export_laura_ner.bat
+++ export_laura_ner.bat
@@ -1,29 +1,45 @@
 @echo off
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_16 -conll laura_ner\interview_16.conll -exportConll laura_ner\interview_16_ner.conll -ner -mmaxPath laura_ner\
-
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_23 -conll laura_ner\interview_23.conll -exportConll laura_ner\interview_23_ner.conll -ner -mmaxPath laura_ner\
-
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_27 -conll laura_ner\interview_27.conll -exportConll laura_ner\interview_27_ner.conll -ner -mmaxPath laura_ner\
-
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_38 -conll laura_ner\interview_38.conll -exportConll laura_ner\interview_38_ner.conll -ner -mmaxPath laura_ner\
-
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_43 -conll laura_ner\interview_43.conll -exportConll laura_ner\interview_43_ner.conll -ner -mmaxPath laura_ner\
-
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_46 -conll laura_ner\interview_46.conll -exportConll laura_ner\interview_46_ner.conll -ner -mmaxPath laura_ner\
+REM java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_16 -conll data\interview_16.conll -exportConll data\interview_16_ner.conll -ner -mmaxPath data\
+REM java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_23 -conll data\interview_23.conll -exportConll data\interview_23_ner.conll -ner -mmaxPath data\
+REM java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_27 -conll data\interview_27.conll -exportConll data\interview_27_ner.conll -ner -mmaxPath data\
+REM java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_38 -conll data\interview_38.conll -exportConll data\interview_38_ner.conll -ner -mmaxPath data\
+REM java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_43 -conll data\interview_43.conll -exportConll data\interview_43_ner.conll -ner -mmaxPath data\
+REM java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_46 -conll data\interview_46.conll -exportConll data\interview_46_ner.conll -ner -mmaxPath data\
 
 
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project G_2005_Ugales_Baznica -conll laura_ner\G_2005_Ugales_Baznica.conll -exportConll laura_ner\G_2005_Ugales_Baznica_ner.conll -ner -mmaxPath laura_ner\
+REM java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 2007_Dadzis -conll data\2007_Dadzis.conll -exportConll data\2007_Dadzis_ner.conll -ner -mmaxPath data\
+REM java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 1988_Padomju_Jaunatne -conll data\1988_Padomju_Jaunatne.conll -exportConll data\1988_Padomju_Jaunatne_ner.conll -ner -mmaxPath data\
+REM java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 1942_Austrumu_apgabala_raiditaju_grupa -conll data\1942_Austrumu_apgabala_raiditaju_grupa.conll -exportConll data\1942_Austrumu_apgabala_raiditaju_grupa_ner.conll -ner -mmaxPath data\
+REM java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 1935_Muzikas_Apskats -conll data\1935_Muzikas_Apskats.conll -exportConll data\1935_Muzikas_Apskats_ner.conll -ner -mmaxPath data\
+REM java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project G_2005_Ugales_Baznica -conll data\G_2005_Ugales_Baznica.conll -exportConll data\G_2005_Ugales_Baznica_ner.conll -ner -mmaxPath data\
+REM java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project G_1966_Oskars_Kalpaks -conll data\G_1966_Oskars_Kalpaks.conll -exportConll data\G_1966_Oskars_Kalpaks_ner.conll -ner -mmaxPath data\
+REM java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project G_1934_Madona -conll data\G_1934_Madona.conll -exportConll data\G_1934_Madona_ner.conll -ner -mmaxPath data\
 
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project G_1966_Oskars_Kalpaks -conll laura_ner\G_1966_Oskars_Kalpaks.conll -exportConll laura_ner\G_1966_Oskars_Kalpaks_ner.conll -ner -mmaxPath laura_ner\
 
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project G_1934_Madona -conll laura_ner\G_1934_Madona.conll -exportConll laura_ner\G_1934_Madona_ner.conll -ner -mmaxPath laura_ner\
-
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 2007_Dadzis -conll laura_ner\2007_Dadzis.conll -exportConll laura_ner\2007_Dadzis_ner.conll -ner -mmaxPath laura_ner\
-
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 1988_Padomju_Jaunatne -conll laura_ner\1988_Padomju_Jaunatne.conll -exportConll laura_ner\1988_Padomju_Jaunatne_ner.conll -ner -mmaxPath laura_ner\
-
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 1942_Austrumu_apgabala_raiditaju_grupa -conll laura_ner\1942_Austrumu_apgabala_raiditaju_grupa.conll -exportConll laura_ner\1942_Austrumu_apgabala_raiditaju_grupa_ner.conll -ner -mmaxPath laura_ner\
-
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 1935_Muzikas_Apskats -conll laura_ner\1935_Muzikas_Apskats.conll -exportConll laura_ner\1935_Muzikas_Apskats_ner.conll -ner -mmaxPath laura_ner\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project cliping_75 -conll data\cliping_75.conll -exportConll data\cliping_75_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project cliping_76 -conll data\cliping_76.conll -exportConll data\cliping_76_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project cliping_77 -conll data\cliping_77.conll -exportConll data\cliping_77_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project cliping_78 -conll data\cliping_78.conll -exportConll data\cliping_78_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project cliping_79 -conll data\cliping_79.conll -exportConll data\cliping_79_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project cliping_80 -conll data\cliping_80.conll -exportConll data\cliping_80_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project cliping_81 -conll data\cliping_81.conll -exportConll data\cliping_81_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project cliping_82 -conll data\cliping_82.conll -exportConll data\cliping_82_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project cliping_83 -conll data\cliping_83.conll -exportConll data\cliping_83_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project cliping_84 -conll data\cliping_84.conll -exportConll data\cliping_84_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_81 -conll data\interview_81.conll -exportConll data\interview_81_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project interview_82 -conll data\interview_82.conll -exportConll data\interview_82_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project lvvest_88 -conll data\lvvest_88.conll -exportConll data\lvvest_88_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project lvvest_89 -conll data\lvvest_89.conll -exportConll data\lvvest_89_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project news_75 -conll data\news_75.conll -exportConll data\news_75_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project news_76 -conll data\news_76.conll -exportConll data\news_76_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project news_77 -conll data\news_77.conll -exportConll data\news_77_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project news_78 -conll data\news_78.conll -exportConll data\news_78_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project news_79 -conll data\news_79.conll -exportConll data\news_79_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project news_80 -conll data\news_80.conll -exportConll data\news_80_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project pr_rel_75 -conll data\pr_rel_75.conll -exportConll data\pr_rel_75_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project pr_rel_76 -conll data\pr_rel_76.conll -exportConll data\pr_rel_76_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project pr_rel_77 -conll data\pr_rel_77.conll -exportConll data\pr_rel_77_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project pr_rel_78 -conll data\pr_rel_78.conll -exportConll data\pr_rel_78_ner.conll -ner -mmaxPath data\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project pr_rel_79 -conll data\pr_rel_79.conll -exportConll data\pr_rel_79_ner.conll -ner -mmaxPath data\
 
 pause
--- import_fromWebNE.bat
+++ import_fromWebNE.bat
@@ -1,17 +1,17 @@
 @echo off
 
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project G_2005_Ugales_Baznica -importWebNE data\fromWebNE\start_data\G_2005_Ugales_Baznica.txt -mmaxPath mmax\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project G_2005_Ugales_Baznica -importWebNE data\fromWebNE\start_data\G_2005_Ugales_Baznica.txt -mmaxPath data\
 
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project G_1966_Oskars_Kalpaks -importWebNE data\fromWebNE\start_data\G_1966_Oskars_Kalpaks.txt -mmaxPath mmax\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project G_1966_Oskars_Kalpaks -importWebNE data\fromWebNE\start_data\G_1966_Oskars_Kalpaks.txt -mmaxPath data\
 
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project G_1934_Madona -importWebNE data\fromWebNE\start_data\G_1934_Madona.txt -mmaxPath mmax\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project G_1934_Madona -importWebNE data\fromWebNE\start_data\G_1934_Madona.txt -mmaxPath data\
 
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 2007_Dadzis -importWebNE data\fromWebNE\start_data\2007_Dadzis.txt -mmaxPath mmax\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 2007_Dadzis -importWebNE data\fromWebNE\start_data\2007_Dadzis.txt -mmaxPath data\
 
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 1988_Padomju_Jaunatne -importWebNE data\fromWebNE\start_data\1988_Padomju_Jaunatne.txt -mmaxPath mmax\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 1988_Padomju_Jaunatne -importWebNE data\fromWebNE\start_data\1988_Padomju_Jaunatne.txt -mmaxPath data\
 
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 1942_Austrumu_apgabala_raiditaju_grupa -importWebNE data\fromWebNE\start_data\1942_Austrumu_apgabala_raiditaju_grupa.txt -mmaxPath mmax\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 1942_Austrumu_apgabala_raiditaju_grupa -importWebNE data\fromWebNE\start_data\1942_Austrumu_apgabala_raiditaju_grupa.txt -mmaxPath data\
 
-java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 1935_Muzikas_Apskats -importWebNE data\fromWebNE\start_data\1935_Muzikas_Apskats.txt -mmaxPath mmax\
+java -Xmx1G -Dfile.encoding=UTF-8 -cp "dist\MmaxWrapper.jar;lib\*" MmaxWrapper %* -project 1935_Muzikas_Apskats -importWebNE data\fromWebNE\start_data\1935_Muzikas_Apskats.txt -mmaxPath data\
 
 pause
